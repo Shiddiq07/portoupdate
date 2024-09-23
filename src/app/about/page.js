354495729f967df51ff6531f5fe5bc9f491b9ebd
Shiddiq07 <mshiddiqs130@gmail.com>
2024-09-23 21:00:27 +0700

    ketiga

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,15 @@
+import profileImage from '../../assets/tailwind.png';
+
 export default function About() {
     return (
       <div className="flex bg-gradient-to-r from-cyan-500 to-blue-500 w-auto" >
   <div class="flex-auto w-32 ">
-    <img src="../app/bgshiddiq.png" alt="contoh profil shiddiq" className="w-1/2 h-auto rounded-lg shadow-lg"/>
- <h1> Muhammad Shiddiq Baihaqi</h1>
+    <img src={profileImage} alt="contoh profil shiddiq" className="w-64 h-auto rounded-lg shadow-lg"/>
+ <h1 className='text-4xl'> Muhammad Shiddiq Baihaqi</h1>
   </div>
  
   <div className="flex-auto w-64">
-    Sedang menempuh perkuliahan di LP3I Jakarta
+   <h2> Sedang menempuh perkuliahan di LP3I Jakarta</h2>
   </div>
 </div>
     );
