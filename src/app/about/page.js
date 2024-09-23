9a2bce6871775107f57569b6cfe77e9a3a522464
Shiddiq07 <mshiddiqs130@gmail.com>
2024-09-23 20:46:55 +0700

    kedua

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,13 @@
 export default function About() {
     return (
-      <div className="flex bg-gradient-to-r from-cyan-500 to-blue-500" >
+      <div className="flex bg-gradient-to-r from-cyan-500 to-blue-500 w-auto" >
   <div class="flex-auto w-32 ">
-    01
+    <img src="../app/bgshiddiq.png" alt="contoh profil shiddiq" className="w-1/2 h-auto rounded-lg shadow-lg"/>
+ <h1> Muhammad Shiddiq Baihaqi</h1>
   </div>
  
   <div className="flex-auto w-64">
-    03
+    Sedang menempuh perkuliahan di LP3I Jakarta
   </div>
 </div>
     );
