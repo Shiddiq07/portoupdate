1ad82c499ccf5f1c2ee83e59db774df0844087ca
Shiddiq07 <mshiddiqs130@gmail.com>
2024-09-23 20:30:34 +0700

    pertama

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+      <div className="flex bg-gradient-to-r from-cyan-500 to-blue-500" >
+  <div class="flex-auto w-32 ">
+    01
+  </div>
+ 
+  <div className="flex-auto w-64">
+    03
+  </div>
+</div>
+    );
+}
+  
