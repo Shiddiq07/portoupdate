914db161ac09d37ef196ecba27698aa5142bb0d8
muhammadshiddiq <mshiddiqs13@gmailcom>
2024-09-24 16:02:53 +0700

    update 1.0

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,9 +2,8 @@
 
 export default function About() {
     return (
-      <div className="flex bg-gradient-to-r from-cyan-500 to-blue-500 w-auto" >
+      <div className="flex w-auto" >
   <div class="flex-auto w-32 ">
-    <img src="" alt="contoh profil shiddiq" className="w-64 h-auto rounded-lg shadow-lg"/>
  <h1 className='text-4xl'> Muhammad Shiddiq Baihaqi</h1>
  <h2 className='text-2xl'> Sedang menempuh perkuliahan di LP3I Jakarta</h2>
   </div>
